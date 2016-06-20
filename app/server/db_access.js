e2b40d387630213dba53ca4afd0bb07490ef0c1f
Paul K <paul.kore@gmail.com>
2016-06-19 20:00:29 -0400

    remove unused function fetchRows()

--- app/server/db_access.js
+++ app/server/db_access.js
@@ -28,32 +28,4 @@ function connect(errorCallback, successCallback) {
     });
 }
 
-/**
- * Establishes a connection and fetches rows returned from an SQL query.
- *
- * @param sql The text of the query with parameter placeholders, if any ($1, $2, $3, etc.)
- * @param params The parameter values to substitute into the placeholders (as array)
- *
- * @param successCallback The function to call on success (result rows array is passed as argument)
- * @param errorCallback The function to call on error (error message passed as argument)
- *
- * TODO: looks like this function is unnecessary
- */
-function fetchRows(sql, params, successCallback, errorCallback) {
-    pg.connect(dbURL, function(err, client) {
-        if (err) {
-            errorCallback('Database error: ' + err.message);
-        }
-        else {
-            client
-                .query(sql, params)
-                .on('row', function (row, result) {
-                    result.addRow(row);
-                })
-                .on('end', function (result) {
-                    successCallback(result.rows);
-                });
-        }
-    });
-}
 
